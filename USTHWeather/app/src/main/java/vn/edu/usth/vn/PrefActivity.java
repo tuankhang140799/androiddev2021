93aac719c4a7ab0f644335f8ee3a1e57ae64a76f
Tuan Khang <tuankhang140799@gmail.com>
2021-01-24 16:56:05 +0700

    Upload Practical Work 12 - 14

---
+++ USTHWeather/app/src/main/java/vn/edu/usth/vn/PrefActivity.java
@@ -0,0 +1,22 @@
+package vn.edu.usth.vn;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class PrefActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pref);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
--- USTHWeather/app/src/main/java/vn/edu/usth/vn/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/vn/WeatherActivity.java
@@ -1,9 +1,18 @@
 package vn.edu.usth.vn;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -38,6 +47,7 @@ public class WeatherActivity extends AppCompatActivity {
         ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
         viewPager.setOffscreenPageLimit(3);
         viewPager.setAdapter(adapter);
+
         TabLayout tabLayout = (TabLayout) findViewById(R.id.pager_header);
         tabLayout.setupWithViewPager(viewPager);
 
@@ -83,6 +93,62 @@ public class WeatherActivity extends AppCompatActivity {
         Log.i("WeatherActivity", "Resuming");
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.refresh:
+                @SuppressLint("StaticFieldLeak") AsyncTask<String, Integer, String> task = new AsyncTask<String, Integer, String>() {
+                    @Override
+                    protected void onPreExecute() {
+                        // do some preparation here, if needed
+                    }
+
+                    @Override
+                    protected String doInBackground(String... params) {
+                        // This is where the worker thread's code is executed
+                        // params are passed from the execute() method call
+                        try {
+                            // wait for 5 seconds to simulate a long network access
+                            Thread.sleep(2000);
+                        }
+                        catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        return params[0];
+                    }
+                    @Override
+                    protected void onProgressUpdate(Integer... values) {
+                        // This method is called in the main thread, so it's possible
+                        // to update UI to reflect the worker thread progress here.
+                        // In a network access task, this should update a progress bar
+                        // to reflect how many percent of data has been retrieved
+                    }
+                    @Override
+                    protected void onPostExecute(String string) {
+                        // This method is called in the main thread. After #doInBackground returns
+                        // the String data, we simply set it to an ImageView using ImageView.setImageBitmap()
+                        Toast.makeText(getApplicationContext(), string, Toast.LENGTH_SHORT).show();
+                    }
+                };
+                task.execute("some sample json here");
+                break;
+            case R.id.action_settings:
+                Intent pref = new Intent(this, PrefActivity.class);
+                startActivity(pref);
+                break;
+            default:
+                super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
+
     private void copyFileToExternalStorage(int resourceId, String resourceName) {
         try {
             File file = new File(getExternalFilesDir(null), resourceName);
@@ -96,8 +162,6 @@ public class WeatherActivity extends AppCompatActivity {
                 }
             }
             finally {
-                Toast toast = Toast.makeText(getApplicationContext(), file.getAbsolutePath(), Toast.LENGTH_LONG);
-                toast.show();
                 in.close();
                 out.close();
             }
@@ -110,4 +174,6 @@ public class WeatherActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
+
+
 }
